cdb7a4e3bb89ef3a0d9222a739c54ab3e6f8a1f4
thetaungg <thetaung@nuzay.com>
2021-01-05 00:49:44 +0630

    Updates

---
+++ component---src-pages-404-tsx-eb64e44aaaa6316ac345.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(t,n,e){"use strict";e.r(n);e("q1tI");var o=e("YtSc"),u=e("qKvR");n.default=function(){return Object(u.a)(o.a,{title:"404: Not found"},Object(u.a)("h1",null,"404: Not Found"),Object(u.a)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-eb64e44aaaa6316ac345.js.map
